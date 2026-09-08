8238feedcd1fe983c36889c3f9d5844488f4ab88
ArthurGaldinoCOTIL <cl206620@g.unicamp.br>
2026-09-08 09:43:29 -0300

    Adiciona conversão inversa de Fahrenheit para Celsius

--- celsius.c
+++ celsius.c
@@ -2,6 +2,8 @@
 int main() {
 float celsius, fahrenheit;
 printf("Digite a temperatura em Celsius: ");
+float celsius_volta = (fahrenheit - 32) * 5.0 / 9.0;
+printf("E %.1f Fahrenheit equivale a %.1f Celsius\n", fahrenheit, celsius_volta);
 scanf("%f", &celsius);
 fahrenheit = (celsius * 9.0 / 5.0) + 32;
 printf("%.1f Celsius equivale a %.1f Fahrenheit\n", celsius, fahrenheit);
